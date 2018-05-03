b4923df2db01e465b85f165c1e7c4120279b2945
Austin Gits Gud <austin@finlinson.net>
2018-05-03 08:29:41 -0600

    Adding dev helpers
    Using reloading and stacktrack for easier Development

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [ring/ring-devel "1.6.3"]]
   :main ^:skip-aot support-form-challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/support_form_challenge/core.clj
+++ src/support_form_challenge/core.clj
@@ -1,13 +1,20 @@
 (ns support-form-challenge.core
-  (:require [ring.adapter.jetty :as j]))
+  (:require [ring.adapter.jetty :as j]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.stacktrace :refer [wrap-stacktrace]]))
 
 (defn handler [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+   :body "Hello World?"})
+
+(def app
+  (-> #'handler
+      (wrap-reload '(support-form-challenge.core))
+      (wrap-stacktrace)))
 
 (defn boot []
-  (j/run-jetty handler {:port 8080}))
+  (j/run-jetty #'app {:port 8080}))
 
 (defn -main
   "I just start up a server"
