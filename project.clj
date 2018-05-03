89f0caa5ca2ad3a0a512ada9fa2ece5a7b2d0b29
Austin Gits Gud <austin@finlinson.net>
2018-05-03 15:06:01 -0600

    Adding a Sqlite database

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
                  [ring/ring-core "1.6.3"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [ring/ring-devel "1.6.3"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.xerial/sqlite-jdbc "3.21.0.1"]]
   :main ^:skip-aot support-form-challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/support_form_challenge/sql.clj
+++ src/support_form_challenge/sql.clj
@@ -1,5 +1,22 @@
 (ns support-form-challenge.sql
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.java.jdbc :as sql]))
+
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname "test.db"})
+
+(defn remake-table [target-db]
+  (sql/execute! target-db ["drop table if exists requests"])
+  (let [creation-script (sql/create-table-ddl
+                          :requests
+                          [[:id :integer :primary :key :autoincrement]
+                           [:category :text :not :null]
+                           [:email :text :not :null]
+                           [:message :text :not :null]
+                           [:file :blob]])]
+    (sql/execute! db [creation-script])))
+(remake-table db)
 
 (def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
 (s/def ::email-type (s/and string? #(re-matches email-regex %)))
@@ -11,7 +28,12 @@
 
 (s/def ::form-data (s/keys :req-un [::email ::category ::message-body]
                            :opt-un [::file]))
-(defn store [form-data]
+(defn store [{:keys [email category message-body file] :as form-data}]
   {:pre [(s/valid? ::form-data form-data)]}
-  (println form-data))
+  (sql/insert! db :requests {:category category
+                             :message message-body
+                             :email email
+                             :file file}))
 
+(defn get-all []
+  (sql/query db ["select id, category, email, message, file from requests"]))
