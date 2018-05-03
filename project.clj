752f762fa4df23d562182a14ab6aba6307e161c8
Austin Gits Gud <austin@finlinson.net>
2018-05-03 08:05:54 -0600

    Adding Ring

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "An example support form webpage for Banzai"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :main ^:skip-aot support-form-challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/support_form_challenge/core.clj
+++ src/support_form_challenge/core.clj
@@ -1,7 +1,16 @@
 (ns support-form-challenge.core
-  (:gen-class))
+  (:require [ring.adapter.jetty :as j]))
+
+(defn handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn boot []
+  (j/run-jetty handler {:port 8080}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I just start up a server"
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (boot))
