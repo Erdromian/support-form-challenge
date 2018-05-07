1b8b4784a471366c1172fe83beba622b7aba7f5a
Austin Gits Gud <austin@finlinson.net>
2018-05-07 12:45:47 -0600

    Tabs

--- src/support_form_challenge/templates.clj
+++ src/support_form_challenge/templates.clj
@@ -14,21 +14,21 @@
     [:div
      [:h1 "Support Page"]
      (with-enctype "multipart/form-data"
-                   (f/form-to [:post ""]
-                              (f/label "category" "Support Category")
-                              [:br]
-                              (f/drop-down "category" categories)
-                              [:br]
-                              (f/label "message" "How can we help?")
-                              [:br]
-                              (f/text-area "message")
-                              [:br]
-                              (f/label "file" "Picture of the issue?")
-                              [:br]
-                              (f/file-upload "file")
-                              [:br]
-                              (f/label "email" "Enter your Email")
-                              [:br]
-                              (f/email-field "email")
-                              [:br]
-                              (f/submit-button "Submit")))]))
+       (f/form-to [:post ""]
+         (f/label "category" "Support Category")
+         [:br]
+         (f/drop-down "category" categories)
+         [:br]
+         (f/label "message" "How can we help?")
+         [:br]
+         (f/text-area "message")
+         [:br]
+         (f/label "file" "Picture of the issue?")
+         [:br]
+         (f/file-upload "file")
+         [:br]
+         (f/label "email" "Enter your Email")
+         [:br]
+         (f/email-field "email")
+         [:br]
+         (f/submit-button "Submit")))]))
